35d8c33fef90ee7dca220e272f1a5dae68c98b8c
Valter Filho <valter.filho7@aluno.enova.educacao.ba.gov.br>
2024-09-30 09:46:05 -0300

    Criação do arquivo php e css da página inicial do funcionário.

---
+++ PHP/homeFuncionario.php
@@ -0,0 +1,96 @@
+<?php 
+
+    require "./sessao/sessao.php";
+
+    $sessaoFuncionario = new Sessao();
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Beatriz Reis e Valter Filho">
+    <meta name="description" content="Site de automoção da Dacal">
+    <title>Dacal</title>
+    <link rel="stylesheet" href="../CSS/homeFuncionario.css">
+</head>
+<header>
+    <div class="informativo_superior">
+        <p>A EMPRESA QUE AUTOMATIZA O PEDIDO DOS SEUS ORÇAMENTOS</p>
+    </div>
+
+    <nav class="nav-superior">
+        <img class="logoDacal" src="../IMAGENS/Homepage/logoDacal.png">
+
+        <ul class="nav-list">
+            <li><a href="">Homepage</li></a>
+            <li><a href="">Catálogo</li></a>
+            <li><a href="">Sobre Nós</li></a>
+        </ul>
+        <ul class="icons">
+            <a href="./autenticacao/logout.php">
+                <button class="sair" href="/IMAGENS/Homepage/logoDacal.png">
+                <img src="../IMAGENS/HomeEmpresa/sair.png" class="sair">
+            </button>
+            </a>
+        </ul>
+    </nav>
+</header>
+
+<body class="fundo">
+    <div class="homepage">
+        <div class="menu">
+            <br>
+            <br>
+            <a class="abas">
+                <img src="../IMAGENS/HomeEmpresa/imgUser.png" class="imgPerfil">
+                <div id="info">
+                    <p>Bem-vindo(a),</p>
+                    <p id="nomeEmpresa"> <?php echo $sessaoFuncionario->getValorSessao('nome'); ?> </p>
+                    <button class="sairInfo" href="">
+                        <img src="../IMAGENS/HomeEmpresa/sair.png" id="imgInfo" alt="">
+                    </button>
+                </div>
+            </a>
+            <br>
+            <hr id="linhaMenu">
+            <br>
+            <a class="abas">
+                <img src="../IMAGENS/HomeEmpresa/imgPerfil.png" class="imgPerfil">
+                <div id="info">
+                    <p class="tituloAbas"> Meu Perfil</p>
+                    <p class="descricaoAbas">Visualize e altere seus</p>
+                    <p class="descricaoAbas">dados.</p>
+                </div>
+            </a>
+        </div>
+        <section class="quadrado">
+            <div id="textoPrincipal">
+                <div id="infoPrincipal">
+                    
+                    <h1 id="tituloFuncionalidades"> Funcionalidades </h1>
+                    <br>
+
+                    <div class="containerFuncionalidades">
+                        <a href=""> <h2> Gerenciar catálogo </h2> </a>
+                    </div>
+                    <div class="containerFuncionalidades">
+                        <a href=""> <h2> Cadastrar produtos </h2> </a>
+                    </div>
+                    <div class="containerFuncionalidades">
+                        <a href=""> <h2> Visualizar orçamentos </h2> </a>
+                    </div>
+                    
+                </div>
+            </div>
+        </section>
+    </div>
+
+
+</body>
+
+</html>
