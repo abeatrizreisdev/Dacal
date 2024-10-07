72934e38a924324eb535e8a8c4c4515e7bc045ff
Valter Filho <valter.filho7@aluno.enova.educacao.ba.gov.br>
2024-10-07 11:42:38 -0300

    Criação do arquivo responsável por pegar os dados de entrada do cadastro do cliente e inserir esses dados em sua tabela no banco de dados.

---
+++ PHP/crud/receberFormulariosDeCadastros/enviarDadosCadastroCliente.php
@@ -0,0 +1,82 @@
+<?php 
+
+    require "../../conexaoBD/conexaoBD.php";
+    require "../crudCliente.php";
+    require "../../sessao/sessao.php";
+    require "../../entidades/cliente.php";
+
+    $conexao = new ConexaoBD();
+    $conexao->setEschemaBD("dacal");
+    $conexao->setHostBD("localhost");
+    $conexao->setPortaBD(3306);
+    $conexao->setEschemaBD("dacal");
+    $conexao->setSenhaBD("96029958va");
+    $conexao->setUsuarioBD("root");
+    $conexao->getConexao(); // Iniciando a conexão com o banco.
+
+    $crudCliente = new CrudCliente($conexao);
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        
+        // Pegando os valores dos campos de entradas do formulário de cadastro de cliente e atribuindo-os as suas variáveis.
+        $id = $_POST['id'];
+        $nome = $_POST['nome'];
+        $razaoSocial = $_POST['razaoSocial'];
+        $cnpj = $_POST['cnpj'];
+        $inscricaoEstadual = $_POST['inscricaoEstadual'];
+        $telefone = $_POST['telefone'];
+        $email = $_POST['email'];
+        $senha = $_POST['senha'];
+        $tipoConta = 'cliente';
+        $logradouro = $_POST['logradouro'];
+        $bairro = $_POST['bairro'];
+        $cep = $_POST['cep'];
+        $estado = $_POST['estado'];
+        $municipio = $_POST['municipio'];
+        $numeroEndereco = $_POST['numeroEndereco'];
+
+        try {
+
+            // Instanciando o objeto que representa o cliente e passando os valores recebidos do formulário de cadastro...
+            // para o objeto do tipo cliente.
+            $cliente = new Cliente();
+            $cliente->setNome($nome);
+            $cliente->setRazaoSocial($razaoSocial);
+            $cliente->setCnpj($cnpj);
+            $cliente->setInscricaoEstadual($inscricaoEstadual);
+            $cliente->setTelefone($telefone);
+            $cliente->setEmail($email);
+            $cliente->setSenha($senha);
+            $cliente->setTipoConta($tipoConta);
+            $cliente->setLogradouro($logradouro);
+            $cliente->setBairro($bairro);
+            $cliente->setCep($cep);
+            $cliente->setEstado($estado);
+            $cliente->setMunicipio($municipio);
+            $cliente->setNumeroEndereco($numeroEndereco);
+
+            // Enviando os dados do cliente que será cadastrado para o banco de dados.
+            $cadastroRealizado = $crudCliente->cadastrarCliente(['nomeEmpresa' => $cliente->getNome(), 'razaoSocial' => $cliente->getRazaoSocial(), 'cnpj' => $cliente->getCnpj(), 'inscricaoEstadual' => $cliente->getInscricaoEstadual(), 'telefone' => $cliente->getTelefone(), 'email' => $cliente->getEmail(), 'senha' => $cliente->getEmail(), 'logradouro' => $cliente->getLogradouro(), 'bairro' => $cliente->getBairro(), 'cep' => $cliente->getCep(), 'estado' => $cliente->getEstado(), 'municipio' => $cliente->getMunicipio(), 'numeroEndereco' => $cliente->getNumeroEndereco()]);
+
+            // Se o cadastro foi efetuado com sucesso, então entrará nessa condicional pois será retornado true.
+            if ($cadastroRealizado) {
+
+                // Vai direcionar o usuário para a página de login.
+                header("Location: .../login.php");
+                exit();
+
+            } 
+
+        } catch(Exception $excecao) {
+
+            echo "Erro ao cadastrar o cliente: " . $excecao->getMessage();
+
+        }
+
+    } else {
+
+        echo 'Requisição inválida.';
+
+    }
+
+?>
