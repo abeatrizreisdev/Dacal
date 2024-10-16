3d0b1c5ce1bad923e0cc4ce9ac77f488c2ff2f3e
Valter Filho <valter.filho7@aluno.enova.educacao.ba.gov.br>
2024-10-16 12:08:52 -0300

    Criação do arquivo responsável por realizar a funcionalidade de exibir o orçamento atual para revisão no passo 2 da funcionalidade de Realizar Orçamento.

--- PHP/realizarOrcamento.php
+++ PHP/realizarOrcamento.php
@@ -1,56 +1,6 @@
 <?php 
 
-require "./sessao/sessao.php";
-require_once "./entidades/orcamento.php";
-require "./entidades/produto.php";
-
-$sessaoCliente = new Sessao();
-
-function exibirOrcamento() {
-    global $sessaoCliente;
-    $orcamento_serializado = $sessaoCliente->getValorSessao('orcamento');
-    if ($orcamento_serializado && is_string($orcamento_serializado)) {
-        error_log("Unserializing orcamento: $orcamento_serializado");
-        $orcamento = unserialize($orcamento_serializado);
-    } else {
-        error_log("Orcamento not found or not a serialized string. Creating new Orcamento.");
-        $orcamento = new Orcamento();
-    }
-
-    if (empty($orcamento->getProdutos())) {
-        echo '<p>Nenhum produto adicionado ao orçamento.</p>';
-        return;
-    }
-
-    $total = 0;
-    $quantidadeTotal = 0;
-    foreach ($orcamento->getProdutos() as $produto) {
-        $quantidade = $orcamento->getQuantidadeProdutos()[$produto->getId()];
-        $total += $produto->getValor() * $quantidade;
-        $quantidadeTotal += $quantidade;
-        $imagem_base64 = base64_encode($produto->getImagem());
-        echo "<div class='produto' id='produto-" . $produto->getId() . "'>
-                <p class='produto-nome'>Produto: " . htmlspecialchars($produto->getNome()) . "</p>
-                <p class='produto-categoria'>Categoria: " . htmlspecialchars($produto->getCategoria()) . "</p>
-                <img class='produto-imagem' src='data:image/jpeg;base64," . $imagem_base64 . "' alt='Produto'>
-                <p class='produto-quantidade'>
-                    Quantidade: 
-                    <button type='button' onclick='alterarQuantidade(" . $produto->getId() . ", -1)'>-</button>
-                    <input type='number' name='quantidades[]' data-produto-id='" . $produto->getId() . "' value='" . htmlspecialchars($quantidade) . "' min='1' readonly>
-                    <button type='button' onclick='alterarQuantidade(" . $produto->getId() . ", 1)'>+</button>
-                </p>
-                <p class='produto-valor'>Valor: R$ <span class='valor'>" . htmlspecialchars($produto->getValor()) . "</span></p>
-                <button class='btn-remover' onclick=\"removerProduto('" . $produto->getId() . "')\">Remover Produto</button>
-                <button class='btn-visualizar' onclick=\"visualizarProduto('" . $produto->getId() . "')\">Visualizar Produto</button>
-              </div>";
-    }
-    echo "<h3>Total: R$ <span id='total'>" . htmlspecialchars($total) . "</span></h3>";
-    echo "<h3>Quantidade Total de Itens: <span id='quantidadeTotal'>" . htmlspecialchars($quantidadeTotal) . "</span></h3>";
-    echo "<button onclick=\"avancarParaPasso3()\">Avançar para o Passo 3</button>";
-}
-
-
-
+    require "./exibirOrcamento/exibirOrcamento.php"
 
 ?>
 
@@ -148,7 +98,7 @@ function exibirOrcamento() {
                     <div id="passo1" class="conteudoPassoAPasso">
 
                         <div class="containerPesquisar">
-                            <input type="text" id="buscarProdutoId" placeholder="Digite o ID do produto">
+                            <input type="text" id="buscarProdutoId" placeholder="Digite o código do produto">
                             <button onclick="buscarProdutoPorId()">Buscar</button>
                         </div>
                         <nav>
