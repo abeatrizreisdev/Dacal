54418a52c130662bd61e40ed49562103a5bae7ef
Beatriz Reis <abeatrizreis1@gmail.com>
2024-10-20 23:28:35 -0300

    formulario de cadastrar funcionario esta com todos os campos necessarios

--- JS/cadastrarFuncionario/cadastrarFuncionario.js
+++ JS/cadastrarFuncionario/cadastrarFuncionario.js
@@ -10,34 +10,67 @@ function abrirPopup() {
 
     // Defina o conteúdo do pop-up
     popup.innerHTML = `
-    <div class="geralCadastro">
-    <h1 class="cadastroTitulo">Formulário de Cadastro de Funcionário</h1>
-    <p class="cadastroGeral">Preencha os campos abaixo:</p>
-    <form>
-        <div class="labelCadastro">
-            <div class="labelOne">
-                <label for="nome">Nome:</label>
-                <input type="text" class="input" name="nome" placeholder="Digite o nome" required>
+              <div class="geralCadastro">
+                <h1 class="cadastroTitulo">Formulário de Cadastro de Funcionário</h1>
+                <p class="cadastroGeral">Preencha os campos abaixo:</p>
+                <form>
+                    <div class="labelCadastro">
+                        <div class="labelOne">
+                            <label for="nome">Nome:</label>
+                            <input type="text" class="input" name="nome" placeholder="Digite o nome" required>
+                        </div>
+                        <div class="labelOne">
+                            <label for="cpf">CPF:</label>
+                            <input type="text" class="input" name="cpf" placeholder="Digite o CPF" required>
+                        </div>
+                    </div>
+                    <div class="labelCadastro">
+                        <div class="labelOne">
+                            <label for="endereco">Telefone:</label>
+                            <input type="text" class="input" name="endereco" placeholder="Digite o numero de telefone"
+                                required>
+                        </div>
+                        <div class="labelOne">
+                            <label for="telefone">Senha:</label>
+                            <input type="tel" class="input" name="telefone" placeholder="Digite o telefone" required>
+                        </div>
+                    </div>
+                    <div class="labelCadastro">
+                        <div class="labelOne">
+                            <label for="endereco">Email:</label>
+                            <input type="text" class="inputE" name="endereco" placeholder="Digite o endereço de E-mail"
+                                required>
+                        </div>
+                    </div>
+                    <br>
+                    <div class="labelCadastro">
+                        <p class="tituloEndereco">Endereço</p>
+                        <input type="text" name="estado" class="inputsAPI" required>
+                        <input type="text" name="municipio" class="inputsAPI" required>
+                    </div>
+                    <div class="labelCadastro">
+                        <div class="labelOne">
+                            <p>Logradouro</p>
+                            <input type="text" class="logradouro" name="logradouro" class="input" placeholder="Digite a rua"required>
+                        </div>
+                        <div class="labelOne">
+                            <p>Nº</p>
+                            <input type="text" class="numeroEndereco" name="numeroEndereco" class="input" placeholder="nº" required>
+                        </div>
+                    </div>
+                    <div class="labelCadastro">
+                        <div class="labelOne">
+                            <p>Bairro</p>
+                            <input type="text" class="bairro" name="bairro" class="input" placeholder="Digite o bairro"required>
+                        </div>
+                        <div class="labelOne">
+                            <p>CEP</p>
+                            <input type="text" class="cep" name="cep" class="input" placeholder="Digite o CEP"required>
+                        </div>
+                    </div>
+                    <button type="submit" class="Cadastrar">Cadastrar Funcionario</button>
+                </form>
             </div>
-            <div class="labelOne">
-                <label for="cpf">CPF:</label>
-                <input type="text" class="input" name="cpf" placeholder="Digite o CPF" required>
-            </div>
-            <div class="labelOne">
-                <label for="telefone">Telefone:</label>
-                 <input type="tel" class="input" name="telefone" placeholder="Digite o telefone" required>
-            </div>
-         </div>
-         <div class="labelCadastro">
-
-             <div class="labelOne">
-                 <label for="endereco">Endereço:</label>
-                 <input type="text" class="Endereco" name="endereco" placeholder="Digite o endereço" required>
-            </div>
-        </div>
-        <button type="submit" class="Cadastrar">Cadastrar Funcionario</button>
-    </form>
-    </div>
     `;
 
     // Adicione o pop-up ao corpo da página
