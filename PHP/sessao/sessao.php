781165a1a7cd3ca130f13f4db0d6f5603a7127df
Valter Filho <valter.filho7@aluno.enova.educacao.ba.gov.br>
2024-11-10 20:31:45 -0300

    Corrigindo um problema com o arquivo de sessão.

--- PHP/sessao/sessao.php
+++ PHP/sessao/sessao.php
@@ -1,56 +1,65 @@
 <?php 
 
-    class Sessao {
+class Sessao {
 
-        public function __construct() {
+    public function __construct() {
 
-            session_start();
+        session_start();
 
-        }
+    }
 
-        public function setChaveEValorSessao($chave, $valor) {
-            $serializedValue = serialize($valor);
-            error_log("Storing in session ($chave): $serializedValue"); // Logging
-            $_SESSION[$chave] = $serializedValue;
-        }
-    
-        public function getValorSessao($chave) {
-            $valor = isset($_SESSION[$chave]) ? $_SESSION[$chave] : null;
-            if ($valor) {
-                // Verificação adicional para garantir que estamos lidando com uma string serializada corretamente
-                if (is_string($valor)) {
-                    error_log("Retrieving from session ($chave): $valor"); // Logging
-                    return unserialize($valor);
-                } else {
-                    error_log("Expected a serialized string but got a different type for session ($chave): " . gettype($valor)); // Logging
-                }
-            }
-            return $valor;
-        }
+    public function setChaveEValorSessao($chave, $valor) {
 
-        public function excluirChaveSessao($chave) {
+        $valorSerializado = serialize($valor);
+        error_log("Armazenando na sessão ($chave): $valorSerializado"); // Logando
+        $_SESSION[$chave] = $valorSerializado;
+        
+    }
 
-            if (isset($_SESSION[$chave])) {
+    public function getValorSessao($chave) {
 
-                unset($_SESSION[$chave]);
+        $valor = isset($_SESSION[$chave]) ? $_SESSION[$chave] : null;
 
-                return true;
+        if ($valor) {
+
+            // Verificação adicional para garantir que estamos lidando com uma string serializada corretamente
+            if (is_string($valor)) {
+
+                return unserialize($valor);
 
             } else {
 
-                return false;
+                error_log("Esperado uma string serializada, mas recebeu um tipo diferente para a sessão ($chave): " . gettype($valor)); // Logando
 
             }
 
         }
 
-        public function encerrarSessao() {
+        return $valor;
+        
+    }
 
-            session_unset();
-            session_destroy();
+    public function excluirChaveSessao($chave) {
+
+        if (isset($_SESSION[$chave])) {
+
+            unset($_SESSION[$chave]);
+
+            return true;
+
+        } else {
+
+            return false;
 
         }
 
     }
 
-?> 
+    public function encerrarSessao() {
+
+        session_unset();
+        session_destroy();
+
+    }
+
+}
